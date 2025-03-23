e6230eac5a879e14291ebb37092ada883bfa1591
menesscesar739 <menesscesar739@gmail.com>
2025-03-23 23:05:08 +0000

    Add 19.php

---
+++ src/19.php
@@ -0,0 +1,7 @@
+<?php
+// This is a simple example of handling an HTTP request and responding with HTML content.
+header("Content-Type: text/html; charset=utf-8");
+echo "<html><body>";
+echo "<h1>Hello, World!</h1>";
+echo "</body></html>";
+?>
