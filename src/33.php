4a3d97b24935a146e91fac6b895071d0e543f4c0
menesscesar739 <menesscesar739@gmail.com>
2025-04-24 19:55:17 +0000

    Add 33.php

---
+++ src/33.php
@@ -0,0 +1,10 @@
+<?php 
+function getRandomString($length = 8) {
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $result = '';
+    for ($i = 0; $i < $length; $i++) {
+        $result .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $result;
+}
+?>
