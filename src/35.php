1095fcd37291590036b5eeeb11568bf5d53c89bf
menesscesar739 <menesscesar739@gmail.com>
2025-04-26 08:24:39 +0000

    Add 35.php

---
+++ src/35.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of PHPMyAdmin.
+ *
+ * PHPMyAdmin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * PHPMyAdmin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PHPMyAdmin.  If not, see <http://www.gnu.org/licenses/>.
+ */
